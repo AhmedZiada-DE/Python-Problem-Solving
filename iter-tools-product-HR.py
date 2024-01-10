cc0e91d197bdba533ac9950c07d88011b5ee0263
Ahmed Ziada <ahmedziada10000@gmail.com>
2024-01-10 13:23:23 +0200

    Addded documentation to iter tools

--- iter-tools-product-HR.py
+++ iter-tools-product-HR.py
@@ -1,5 +1,8 @@
+# The purpose of this code is to get the cartesian product of 2 inputs
 from itertools import product
 
 a = map(int, input().split(' '))
 b = map(int, input().split(' '))
+
+# The astrix * is for unpacking the list so that it can appear without the brackets
 print(*list(product(a,b)))
