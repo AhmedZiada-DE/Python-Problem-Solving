63ef4d2402e63757edfb7684564012c27b79213d
Ahmed Ziada <ahmedziada10000@gmail.com>
2024-01-14 20:05:23 +0200

    added documentatio to the exceptions code

--- exceptions-HR.py
+++ exceptions-HR.py
@@ -1,12 +1,15 @@
+# Number of tests to try
 testNum = int(input())
 test = []
 for i in range(testNum):
     li = input().split(' ')
     test.append(li)
-    
+
+# Input space separated values
 for nums in test:
     try:
         nums = [int(num) for num in nums]
+        # print in case there was no error, else show the error
         print(nums[0]//nums[1])
     except ZeroDivisionError as e:
         print('Error Code:',e)
