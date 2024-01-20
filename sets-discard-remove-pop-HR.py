93b707df7346300de391af3ec3104f7c8c9b1941
Ahmed Ziada <ahmedziada10000@gmail.com>
2024-01-20 23:06:52 +0200

    Solved sets-discard-remove-pop

---
+++ sets-discard-remove-pop-HR.py
@@ -0,0 +1,25 @@
+# Discard, Remove and Pop numbers ranging from 0 to 9 from a set. The user specifies how many commands will be given using the cmd variable
+# Then the sum of the set is printed in the end
+# https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem?isFullScreen=true
+
+n = int(input())
+s = set(map(int, input().split()))
+cmd = int(input())
+a= 0
+for i in range(cmd):
+    userInp = input().split(' ')
+    order = userInp[0]
+    
+    if order == 'remove':
+        try:
+            s.remove(int(userInp[1]))
+        except KeyError:
+            continue
+    
+    elif order == 'discard':
+        s.discard(int(userInp[1]))
+    elif order == 'pop':
+        s.pop()
+    a+=1
+# print(s)
+print(sum(s))
