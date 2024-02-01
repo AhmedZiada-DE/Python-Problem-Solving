dee6c661ec18a138748705bb63f8c05160be99eb
Ahmed Ziada <ahmedziada10000@gmail.com>
2024-02-01 13:20:55 +0200

    Edited itertools-permuatations solution

--- itertools-permutations-HR.py
+++ itertools-permutations-HR.py
@@ -1,18 +1,15 @@
 # print the combinations of a string using variable num to decide the size of the output
 # https://www.hackerrank.com/challenges/itertools-permutations/problem?isFullScreen=true
+
 from itertools import permutations
 str1, num = input().split(' ')
-str1 = [x for x in str1]
-# Other options to convert str1 to separate characters
+# options to convert str1 to separate characters not needed here though
+# str1 = [x for x in str1]
 # str1 = *str1
 # str1 = list(str1)
-# print(str1)
-num = int(num)
-res = list(permutations(str1, num))
-lst = []
-for itr in res:
-    lst.append(''.join(itr))
 
-lst.sort()
-for item in lst:
-    print(item)
+num = int(num)
+res = sorted(list(permutations(str1, num)))
+
+for itr in res:
+    print(''.join(itr))
